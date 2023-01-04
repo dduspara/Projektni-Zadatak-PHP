91258ed5c4598a66594714c899a81b814cc8cc2c
Dominik <92716459+dduspara@users.noreply.github.com>
2023-01-04 09:44:51 +0100

    Add files via upload

---
+++ Projektni-Zadatak-1/news-4.php
@@ -0,0 +1,21 @@
+<?php
+print '
+		<h1>Vijesti</h1>
+		<div id="news_gallery">
+			<figure id="1">
+				<img src="images/news-sl4.jpg" alt="Grad Zagreb" title="Grad Zagreb">
+				<figcaption>Grad Zagreb</figcaption>
+			</figure>
+		</div>
+		<hr>
+		<div class="news">
+			<h2>Predstavljanje 94 mil. kn vrijednog projekta smanjenja komunalnog otpada u Gradu Zagrebu</h2>
+			<p class="description">Grad Zagreb i Zagrebački holding d.o.o., zajedno sa podružnicama Čistoća i Zrinjevac, udružili su snage za čišći Zagreb.</p>
+            <p><time datetime="2021-12-07">7 December 2021</time></p>
+			<p>Grad Zagreb provodi EU projekt „Interventna mjera za smanjenje odlaganja komunalnog otpada nastalog u Gradu Zagrebu“. </p>
+			<p>Projektom će se unaprijediti sustav odvojenog prikupljanja otpada te time postići smanjenje odlaganja miješanog komunalnog otpada sukladno obavezama propisanim Zakonom o održivom gospodarenju otpadom i ciljevima iz Plana gospodarenja otpadom Republike Hrvatske 2017. - 2022.</p>
+			<p>Projekt je sufinancirala Europska unije iz Operativnog programa „Konkurentnost i kohezija 2014. – 2020.“. Ukupna vrijednost projekta je 93.973.317,60 kn, od čega je udio EU sufinanciranja 75.678.598,97, a provodi se od 10. studenog 2015. do 31. prosinca 2022. godine.</p>
+			<p>Projekt ima za cilj unaprjeđenje sustava gospodarenja otpadom kroz smanjenje količina miješanog komunalnog otpada, povećanje stope odvojenog prikupljanja otpada na području Grada Zagreba i doprinos održivom razvoju i održivosti resursa.</p>
+			<p><a href="news.php">Back to news</a></p>
+		</div>';
+?>
---
+++ Projektni-Zadatak-1/news.php
@@ -0,0 +1,31 @@
+<?php
+print '
+		<h1>Vijesti</h1>
+		<div class="news">
+			<a href="news-1.php"><img src="images/news-sl1.jfif" alt="Primopredaja specijalnoga navalnog vatrogasnog vozila" title="Primopredaja specijalnoga navalnog vatrogasnog vozila"></a>
+			<a href="news-1.php"><h2>Primopredaja specijalnoga navalnog vatrogasnog vozila</h2></a>
+			<p>Radi se o vozilu za borbu protiv velikih požara, ali se korist i u hitnim intervencijama, npr. u prometnim nesrećama i drugim akcijama spašavanja, a godišnje zatreba na oko 350 intervencija. <a href="news-1.php">More ...</a></p>
+			<p><time datetime="2021-12-23">23 December 2021</time></p>
+			<hr>
+			<a href="news-2.php"><img src="images/news-sl2.jpg" alt="Novi Hrvatski prirodoslovni muzej po mjeri svjetskih metropola" title="Novi Hrvatski prirodoslovni muzej po mjeri svjetskih metropola"></a>
+			<a href="news-2.php"><h2>Novi Hrvatski prirodoslovni muzej po mjeri svjetskih metropola</h2></a>
+			<p>Danas je u atriju Hrvatskog prirodoslovnog muzeja svečano otvoreno gradilište trenutno najvećeg infrastrukturnog projekta u kulturi Grada Zagreba <a href="news-2.php">More ...</a></p>
+			<p><time datetime="2021-12-20">20 December 2021</time></p>
+			<hr>
+			<a href="news-3.php"><img src="images/news-sl3.jpg" alt="Obnovljeno Prvo i Drugo maksimirsko jezero" title="Obnovljeno Prvo i Drugo maksimirsko jezero"></a>
+			<a href="news-3.php"><h2>Obnovljeno Prvo i Drugo maksimirsko jezero</h2></a>
+			<p>Puštanje vode u obnovljeno Prvo i Drugo maksimirsko jezero u sklopu europskog projekta <a href="news-3.php">More ...</a></p>
+			<p><time datetime="2021-12-17">17 December 2021</time></p>
+			<hr>
+			<a href="news-4.php"><img src="images/news-sl4.jpg" alt="" title=""></a>
+			<a href="news-4.php"><h2>Predstavljanje 94 mil. kn vrijednog projekta smanjenja komunalnog otpada u Gradu Zagrebu</h2></a>
+			<p>Grad Zagreb i Zagrebački holding d.o.o., zajedno sa podružnicama Čistoća i Zrinjevac, udružili su snage za čišći Zagreb. <a href="news-4.php">More ...</a></p>
+			<p><time datetime="2021-12-07">7 December 2021</time></p>
+			<hr>
+			<a href="news-5.php"><img src="images/news-sl5.jfif" alt="" title=""></a>
+			<a href="news-5.php"><h2>Trešnjevka dobila novi objekt Osnovne škole August Šenoa</h2></a>
+			<p>Novoizgrađeni školski objekt sa sportskom dvoranom Područne škole Pongračevo OŠ Augusta Šenoe <a href="news-5.php">More ...</a></p>
+			<p><time datetime="2021-12-03">3 December 2021</time></p>
+			<hr>
+		</div>';
+?>
